9d24101d9625604625c0d9336b2c53d18a314265
Frodobag10 <122989986+Frodobag10@users.noreply.github.com>
2023-01-20 14:18:15 -0600

    stuff

--- main.ts
+++ main.ts
@@ -1,18 +1,251 @@
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+let Anime: Sprite = null
+let ask_clue2 = ""
+let ask_clue1 = ""
+let _2ask_clue2 = ""
+let _2ask_clue1 = ""
+let Mrs_Kerscker: Sprite = null
+let Will: Sprite = null
+let Aubrey: Sprite = null
+let Lauren: Sprite = null
+let Isaiah: Sprite = null
+let Lucie: Sprite = null
+let Levi: Sprite = null
+let Amelia: Sprite = null
+function clue3 () {
+    Anime.sayText("Your third clue is a Ceaser Cipher")
+    pause(3000)
+    Anime.sayText("You have 15 seconds to look at the clue.")
+    pause(3000)
+    Anime.sayText("Aopz wlyzvu pz h mlthsl dpao svun isvuk ohpy dpao ha slhza vul fvbunly iyvaoly.")
+    pause(15000)
+    ask_clue2 = game.askForString("Would you like to guess? Type Y for yes or N for no.", 1)
+    if (ask_clue1 == "Y") {
+        _2ask_clue2 = game.askForString("Who is your guess", 10)
+        if (_2ask_clue1 == "Aubrey") {
+            game.over(true)
+        } else {
+            game.over(false)
+        }
+    } else {
+        clue4()
+    }
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Anime = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    Mrs_Kerscker = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    Will = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    Aubrey = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    Lauren = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    Isaiah = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    Lucie = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    Levi = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    Amelia = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    clue1()
+})
+function clue4 () {
+    Anime.sayText("Your fourth and final clue is in morse code.")
+    pause(3000)
+    Anime.sayText("You have 15 seconds to look at the clue.")
+    pause(15000)
+    Anime.sayText(".- ..- -... .-. . -.-- / .. ... / - .... . / -.- .. -.. -. .- .--. .--. . .-.")
+    ask_clue2 = game.askForString("Who do you think is the criminal?", 1)
+    if (_2ask_clue1 == "Aubrey") {
+        game.over(true)
+    } else {
+        game.over(false)
+    }
+}
+function clue2 () {
+    Anime.sayText("Your second clue is a basic number letter switcher, i.e. 1=a")
+    pause(3000)
+    Anime.sayText("You have 10 seconds to look at the clue.")
+    pause(3000)
+    Anime.sayText("20 8 5 7 21 9 12 20 25 16 5 18 19 15 14 9 19 9 14 1 12 7 5 2 18 1 20 23 15 ")
+    pause(10000)
+    ask_clue2 = game.askForString("Would you like to guess? Type Y for yes or N for no.", 1)
+    if (ask_clue1 == "Y") {
+        _2ask_clue2 = game.askForString("Who is your guess", 10)
+        if (_2ask_clue1 == "Aubrey") {
+            game.over(true)
+        } else {
+            game.over(false)
+        }
+    } else {
+        clue3()
+    }
+}
+function clue1 () {
+    Anime.sayText("Your first clue is an anagram. You have 10 seconds to look at it. ")
+    pause(3000)
+    Anime.sayText("You may want to write it down. Word unscramblers are permitted.")
+    pause(3000)
+    Anime.sayText("wn eteth ernatiormnrgohog nit")
+    pause(10000)
+    ask_clue1 = game.askForString("Would you like to guess? Type Y for yes or N for no.", 1)
+    if (ask_clue1 == "Y") {
+        _2ask_clue1 = game.askForString("Who is your guess", 10)
+        if (_2ask_clue1 == "Aubrey") {
+            game.over(true)
+        } else {
+            game.over(false)
+        }
+    } else {
+        clue2()
+    }
+}
+function gameover () {
+	
+}
